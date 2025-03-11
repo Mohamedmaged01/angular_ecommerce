0571d50304fb225c85b83d6eb4adce8dd278589f
Mostafa <mostafamokna78@gmail.com>
2025-03-12 01:31:30 +0200

    modify category

--- front-end/src/app/admin/categorydetails/categorydetails.component.ts
+++ front-end/src/app/admin/categorydetails/categorydetails.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {SidebarComponent} from '../sidebar/sidebar.component';
 import { RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-categorydetails',
-  imports: [SidebarComponent,RouterLink],
+  imports: [SidebarComponent,RouterLink,NgIf],
   templateUrl: './categorydetails.component.html',
   styleUrl: './categorydetails.component.css'
 })
-export class CategorydetailsComponent {
+export class CategorydetailsComponent implements OnInit {
+  isViewMode: boolean = false;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.isViewMode = params['view'] === 'view'; // If query param 'view' is 'view', disable button
+    });
+  }
 }
