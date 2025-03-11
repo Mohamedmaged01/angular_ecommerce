857607baf9427ca4cc3c49ec73e061bc58f60580
Marwa Nasser <marwa.nasser8133@gmail.com>
2025-03-11 05:40:38 +0200

    Header Done

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< HEAD
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+=======
+import { HeaderComponent } from './user/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent],
+>>>>>>> 93d6775 (Header Done)
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
