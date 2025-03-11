5afd1c8dd310e33ff2e00355a9a8867a743af20f
Mostafa <mostafamokna78@gmail.com>
2025-03-11 19:38:52 +0200

    side bar

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SidebarComponent } from './admin/sidebar/sidebar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:SidebarComponent,
+        title:'side bar'
+    }
+];
