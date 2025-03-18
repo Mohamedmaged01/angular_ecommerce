d76ff9c7d3eb754590d7fa9e0d8518ff1fa5ba00
Mostafa <mostafamokna78@gmail.com>
2025-03-19 00:45:01 +0200

    list & delete

--- front-end/src/app/admin/customer/customer.component.ts
+++ front-end/src/app/admin/customer/customer.component.ts
@@ -1,13 +1,69 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { RouterLink } from '@angular/router';
+import { CustomerService } from '../../services/customer.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customer',
-  imports: [SidebarComponent,RouterLink],
+  imports: [SidebarComponent,RouterLink,CommonModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
-export class CustomerComponent {
+export class CustomerComponent implements OnInit {
 
+  customers: any[] = [];
+  isLoading = false;
+  errorMessage: string | null = null;
+
+  constructor(private customerService: CustomerService) { }
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    console.log('Stored Token:', token);
+    this.loadCustomers();
+  }
+  refreshCategories(): void {
+    this.loadCustomers();
+  }
+  loadCustomers(): void {
+    this.isLoading = true;
+    this.errorMessage = null; 
+  
+    this.customerService.getCustomers().subscribe({
+      next: (response) => {
+        console.log('API Response:', response);
+        this.customers = response.customers || response; 
+        this.isLoading = false;
+        console.log(this.customers)
+      },
+      error: (error) => {
+        console.error('API Error:', error);
+        if (error.status === 401) {
+          this.errorMessage = 'Authentication failed. Please login again.';
+        } else {
+          this.errorMessage = error.error?.message || 'Failed to load customers';
+        }
+        this.isLoading = false;
+      }
+    });
+  }
+
+  deleteCustomers(id:any){
+    this.customerService.deleteCustomer(id).subscribe({
+      next: (response) => {
+        console.log('API Response:', response);
+        this.loadCustomers();
+        },
+      error: (error) => {
+        console.error('API Error:', error);
+        if (error.status === 401) {
+            this.errorMessage = 'Authentication failed. Please login again.';
+        } else {
+            this.errorMessage = error.error?.message || 'Failed to delete customer';
+        }
+        this.isLoading = false;
+      }
+    });
+  }
 }
